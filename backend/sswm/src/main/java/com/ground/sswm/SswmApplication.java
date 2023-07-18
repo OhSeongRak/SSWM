3dbd348babd73b1517b3d6cfd448ab667e8c4be7
박은정 <dms011221@gmail.com>
2023-07-18 10:02:06 +0900

    init project setting

---
+++ backend/sswm/src/main/java/com/ground/sswm/SswmApplication.java
@@ -0,0 +1,13 @@
+package com.ground.sswm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SswmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SswmApplication.class, args);
+	}
+
+}
---
+++ backend/sswm/src/test/java/com/ground/sswm/SswmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ground.sswm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SswmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
