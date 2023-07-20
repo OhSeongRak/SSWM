082c1f14cd43aede93a8bfc979f85fe124118b43
unknown <ssj946@naver.com>
2023-07-20 10:40:54 +0900

    feature: add all userTree

---
+++ backend/sswm/src/main/java/com/ground/sswm/usertree/controller/UserTreeController.java
@@ -0,0 +1,23 @@
+package com.ground.sswm.usertree.controller;
+
+import com.ground.sswm.usertree.domain.UserTree;
+import com.ground.sswm.usertree.service.UserTreeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/user-trees")
+public class UserTreeController {
+    UserTreeService userTreeService;
+
+    @RequestMapping(value = "findAll", method = RequestMethod.GET)
+    public List<UserTree> findAll(){
+        return (List<UserTree>) userTreeService.findAll();
+    }
+}
