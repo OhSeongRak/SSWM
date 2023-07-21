deb579102ee4ea7761bbfad2cc9e0e0f5dfb0140
김재헌 <tint1998@gmail.com>
2023-07-21 15:16:06 +0900

    feat : search user in studyroom api

--- backend/sswm/src/main/java/com/ground/sswm/userStudyroom/dto/UserStudyTimeResDto.java
+++ backend/sswm/src/main/java/com/ground/sswm/userStudyroom/dto/UserStudyTimeResDto.java
@@ -1,13 +1,10 @@
 package com.ground.sswm.userStudyroom.dto;
-
-<<<<<<< HEAD
-public class UserStudyTimeResDto {
-
-=======
 import com.ground.sswm.user.dto.UserDto;
 
 public class UserStudyTimeResDto {
+
     private UserDto userDto;
     private int studyTime;
->>>>>>> 3d396a1accee36fd19d074cfefe027a4e89578ad
+
+
 }
