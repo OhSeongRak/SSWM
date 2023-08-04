ea757198d7516e46fce7bf470c30871daf7cde8b
OhSeongRak <osr0907@naver.com>
2023-08-04 17:32:47 +0900

    add: get all room with axios

--- frontend/sswm-front/src/components/StudyRoom/StudyRoomList.jsx
+++ frontend/sswm-front/src/components/StudyRoom/StudyRoomList.jsx
@@ -2,6 +2,8 @@ import styled from "styled-components";
 import { useInView } from "react-intersection-observer";
 import RecipeReviewCard from "./StudyRoomItem2";
 import "./Style.css";
+import axios from "axios";
+import { useState } from 'react';
 
 const RoomListLayout = styled.div`
   flex: 1;
@@ -16,11 +18,33 @@ const RoomList = styled.ul`
 `;
 
 const StudyRoomList = (props) => {
+  console.log("왜이리 호출할까")
   const [ref] = useInView();
+  const token = JSON.parse(localStorage.getItem("jwtToken"));
+  const data = {
+    "sortBy" : "STUDY_TIME", // 공부시간(STUDY_TIME), 인원(USER_NUM), 생성시간(CREATED), 
+    "orderBy" : "ASC", // ASC
+    "searchKeyword" : "", // 방제목, 방아이디
+    "tagNames" : [
+    ]
+  };
+
+  let studyrooms = []; // 초기화
+
+  axios
+  .post("http://localhost:8080/api/studyrooms/list", data, {
+    headers: {
+      Authorization: token.accessToken,
+    },
+  })
+  .then((response) => {
+    studyrooms = response.data; // API 호출 완료 후에 studyrooms 업데이트
+    console.log("studyrooms::::", response.data);
+  });
+    
   return (
     <RoomListLayout ref={ref}>
       <RoomList>
-        <RecipeReviewCard />
         <RecipeReviewCard />
         <RecipeReviewCard />
         <RecipeReviewCard />
