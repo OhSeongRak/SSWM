84457c6cafe806b8a49c241038ff351072220c89
김현수 <gustn2615@naver.com>
2023-08-09 15:20:28 +0900

    fix: login, webConfig

--- backend/sswm/src/main/java/com/ground/sswm/dailyLog/DailyLogController.java
+++ backend/sswm/src/main/java/com/ground/sswm/dailyLog/DailyLogController.java
@@ -5,16 +5,20 @@ import com.ground.sswm.dailyLog.service.DailyLogService;
 import com.ground.sswm.studyroom.model.dto.StudyroomDto;
 import java.util.Map;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/user-logs")
@@ -25,10 +29,17 @@ public class DailyLogController {
     @PostMapping("/{studyroomId}")
     public ResponseEntity<?> add(@RequestHeader("Authorization") String token,
         @PathVariable Long studyroomId) {
+        log.debug("studyroomId : " +studyroomId);
         Map<String, Object> headerToken = authService.getClaimsFromToken(token);
-        Long userId = (Long) headerToken.get("id");
+        Long userId = Long.valueOf(headerToken.get("id").toString());
+        log.debug("userId :" +userId);
         dailyLogService.create(studyroomId,userId);
         return new ResponseEntity<>("", HttpStatus.OK);
+    }
+
+    @GetMapping
+    public ResponseEntity<?> list(@RequestParam Long start,@RequestParam Long end) {
+        return null;
     }
 
 }
