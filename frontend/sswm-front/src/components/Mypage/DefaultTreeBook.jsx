526d0d0218e5888da6d44bf7d72e23194a7ea26c
박은정 <dms011221@gmail.com>
2023-08-14 17:11:26 +0900

    add: DefaultTreeBook

---
+++ frontend/sswm-front/src/components/Mypage/DefaultTreeBook.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import styled from "styled-components";
+
+
+const DefaultTreeBook = (props) => {
+  return(
+    <ContainerWrap>
+      <h2>나무를 키워보세요</h2>
+    </ContainerWrap>
+  );
+};
+
+const ContainerWrap = styled.div`
+  width: 100%;
+`
+
+
+export default DefaultTreeBook;
