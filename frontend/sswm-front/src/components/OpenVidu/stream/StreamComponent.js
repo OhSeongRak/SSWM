67e20f9c6d0542183adfa07bcb001a859b8b6951
김재헌 <tint1998@gmail.com>
2023-08-16 13:42:48 +0900

    feat : cam and mic icon

--- frontend/sswm-front/src/components/OpenVidu/stream/StreamComponent.js
+++ frontend/sswm-front/src/components/OpenVidu/stream/StreamComponent.js
@@ -2,8 +2,6 @@ import React, { Component } from 'react';
 import './StreamComponent.css';
 import OvVideoComponent from './OvVideo';
 
-import MicOff from '@material-ui/icons/MicOff';
-import VideocamOff from '@material-ui/icons/VideocamOff';
 import FormControl from '@material-ui/core/FormControl';
 import Input from '@material-ui/core/Input';
 import InputLabel from '@material-ui/core/InputLabel';
@@ -117,13 +115,6 @@ export default class StreamComponent extends Component {
                 {this.props.user !== undefined && this.props.user.getStreamManager() !== undefined ? (
                     <div className="streamComponent">
                         <OvVideoComponent user={this.props.user} mutedSound={this.state.mutedSound} localUser={this.props.localUser} onNotificationButtonClick={this.handleNotificationButtonClick} micButtonClick={this.toggleSound}/>
-                        <div id="statusIcons">
-                            {!this.props.user.isVideoActive() ? (
-                                <div id="camIcon">  
-                                    <VideocamOff id="statusCam" />
-                                </div>
-                            ) : null}
-                        </div>
                     </div>
                 ) : null}
             </div>
