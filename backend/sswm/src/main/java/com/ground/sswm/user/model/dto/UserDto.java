6d7b4e5fa9e461211c648d75535fa0dec49a9b40
OhSeongRak <osr0907@naver.com>
2023-08-17 11:30:45 +0900

    fix: no args constructor

--- backend/sswm/src/main/java/com/ground/sswm/user/model/dto/UserDto.java
+++ backend/sswm/src/main/java/com/ground/sswm/user/model/dto/UserDto.java
@@ -3,10 +3,12 @@ package com.ground.sswm.user.model.dto;
 import com.ground.sswm.user.model.User;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
 public class UserDto {
 
     private Long id;
